0085d8160adca587af512bb5d94955d9c79c2c27
Matheus Catarino <matheus-catarino@hotmail.com>
2025-01-26 16:06:28 -0300

    nix updated

--- flake.nix
+++ flake.nix
@@ -6,16 +6,8 @@
     esp-idf.url = "github:mirrexagon/nixpkgs-esp-dev";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , zls-overlay
-    , esp-idf
-    ,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { self, nixpkgs, flake-utils, zls-overlay, esp-idf, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
@@ -23,27 +15,24 @@
             esp-idf.overlays.default
             (final: prev: {
               zls = zls-overlay.packages.${system}.default;
-              zig = pkgs.stdenv.mkDerivation {
-                pname = "zig-espressif-bootstrap";
-                version = "0.14.0-xtensa-dev";
-
-                src = pkgs.fetchurl {
-                  url = "https://github.com/kassane/zig-espressif-bootstrap/releases/download/0.14.0-xtensa-dev/zig-relsafe-espressif-x86_64-linux-musl-baseline.tar.xz";
-                  sha256 = "sha256-KAFGYgUP1poWbUJCsvqrJIs7VyZayWbGDqeD+jUG7og=";
-                };
-                dontConfigure = true;
-                dontBuild = true;
-                dontFixup = true;
-                installPhase = ''
-                  mkdir -p $out/{doc,bin,lib}
-                  cp -r doc/* $out/doc
-                  cp -r lib/* $out/lib
-                  cp zig $out/bin/zig
-                '';
-              };
             })
           ];
         };
+
+        # Determine architecture-specific properties
+        platformSrc = if system == "x86_64-linux" then
+          {
+            url = "https://github.com/kassane/zig-espressif-bootstrap/releases/download/0.14.0-xtensa-dev/zig-relsafe-espressif-x86_64-linux-musl-baseline.tar.xz";
+            sha256 = "sha256-KAFGYgUP1poW79QrLwq7JI07VyZayWbBDqcGT6NQbug=";
+          }
+        else if system == "aarch64-linux" then
+          {
+            url = "https://github.com/kassane/zig-espressif-bootstrap/releases/download/0.14.0-xtensa-dev/zig-relsafe-espressif-aarch64-linux-musl-baseline.tar.xz";
+            sha256 = "sha256-1bnaav5a8gp108izghlcpdnl1y0rl5jzzmzx47q234k3h5m42jb2";
+          }
+        else
+          throw "Unsupported platform: ${system}";
+
       in
       with pkgs;
       {
@@ -52,7 +41,21 @@
           buildInputs = [
             bashInteractive
 
-            zig
+            (pkgs.stdenv.mkDerivation {
+              pname = "zig-espressif-bootstrap";
+              version = "0.14.0-xtensa-dev";
+              src = pkgs.fetchurl platformSrc;
+              dontConfigure = true;
+              dontBuild = true;
+              dontFixup = true;
+              installPhase = ''
+                mkdir -p $out/{doc,bin,lib}
+                cp -r doc/* $out/doc
+                cp -r lib/* $out/lib
+                cp zig $out/bin/zig
+              '';
+            })
+
             zls
 
             esp-idf-full
