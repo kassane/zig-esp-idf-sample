d9591920717fbfcb6ff7b4d7352ce86004b6c5c4
Joseph Montanez <sutabi@gmail.com>
2025-02-09 02:24:38 -0800

    fixed dynamic comptime gpio pins
    Depends on Xtensa / RiscV and series on the number of pins

--- imports/gpio.zig
+++ imports/gpio.zig
@@ -12,7 +12,7 @@ inline fn buildExtendedGPIO(comptime lib: type) []const std.builtin.Type.EnumFie
 
     for ([_]u8{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48 }) |n| {
         const field_name = std.fmt.comptimePrint("GPIO_NUM_{}", .{n});
-        if (@hasField(lib, field_name)) {
+        if (@hasDecl(lib, field_name)) {
             targets = targets ++ &[_]std.builtin.Type.EnumField{
                 .{
                     .name = field_name,
