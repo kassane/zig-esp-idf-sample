4f10845a110fac04c4128e5a64ac8bbea55db7ad
Matheus C. França <matheus-catarino@hotmail.com>
2026-02-03 10:32:27 -0300

    `build.zig` cleanup

--- build.zig
+++ build.zig
@@ -7,126 +7,18 @@ pub fn build(b: *std.Build) !void {
     });
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
+    const lib = b.addLibrary(.{
         .name = "app_zig",
-        .root_source_file = b.path("main/app.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("main/app.zig"),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
     });
-    const src_path = std.fs.path.dirname(@src().file) orelse b.pathResolve(&.{"."});
-    lib.addIncludePath(.{ .cwd_relative = b.pathJoin(&.{ src_path, "build", "config" }) });
     lib.root_module.addImport("esp_idf", idf_wrapped_modules(b));
-    lib.linkLibC(); // stubs for libc
 
-    try includeDeps(b, lib);
     b.installArtifact(lib);
-}
-
-fn includeDeps(b: *std.Build, lib: *std.Build.Step.Compile) !void {
-    const include_dirs = std.process.getEnvVarOwned(b.allocator, "INCLUDE_DIRS") catch "";
-    if (!std.mem.eql(u8, include_dirs, "")) {
-        var it_inc = std.mem.tokenizeAny(u8, include_dirs, ";");
-        while (it_inc.next()) |dir| {
-            lib.addIncludePath(.{ .cwd_relative = dir });
-        }
-    }
-
-    const idf_path = std.process.getEnvVarOwned(b.allocator, "IDF_PATH") catch "";
-    if (!std.mem.eql(u8, idf_path, "")) {
-        try searched_idf_include(b, lib, idf_path);
-        try searched_idf_libs(b, lib);
-    }
-
-    const home_dir = std.process.getEnvVarOwned(b.allocator, "HOME") catch "";
-    if (!std.mem.eql(u8, home_dir, "")) {
-        const archtools = b.fmt("{s}-esp-elf", .{
-            @tagName(lib.rootModuleTarget().cpu.arch),
-        });
-
-        lib.addIncludePath(.{
-            .cwd_relative = b.pathJoin(&.{
-                home_dir,
-                ".espressif",
-                "tools",
-                archtools,
-                "esp-14.2.0_20241119",
-                archtools,
-                "include",
-            }),
-        });
-        lib.addSystemIncludePath(.{
-            .cwd_relative = b.pathJoin(&.{
-                home_dir,
-                ".espressif",
-                "tools",
-                archtools,
-                "esp-14.2.0_20241119",
-                archtools,
-                archtools,
-                "sys-include",
-            }),
-        });
-        lib.addIncludePath(.{
-            .cwd_relative = b.pathJoin(&.{
-                home_dir,
-                ".espressif",
-                "tools",
-                archtools,
-                "esp-14.2.0_20241119",
-                archtools,
-                archtools,
-                "include",
-            }),
-        });
-    }
-
-    // user include dirs
-    lib.addIncludePath(b.path("include"));
-}
-
-pub fn searched_idf_libs(b: *std.Build, lib: *std.Build.Step.Compile) !void {
-    var dir = try std.fs.cwd().openDir("../build", .{
-        .iterate = true,
-    });
-    defer dir.close();
-    var walker = try dir.walk(b.allocator);
-    defer walker.deinit();
-
-    while (try walker.next()) |entry| {
-        const ext = std.fs.path.extension(entry.basename);
-        const lib_ext = inline for (&.{".obj"}) |e| {
-            if (std.mem.eql(u8, ext, e))
-                break true;
-        } else false;
-        if (lib_ext) {
-            const src_path = std.fs.path.dirname(@src().file) orelse b.pathResolve(&.{".."});
-            const cwd_path = b.pathJoin(&.{ src_path, "build", b.dupe(entry.path) });
-            const lib_file: std.Build.LazyPath = .{ .cwd_relative = cwd_path };
-            lib.addObjectFile(lib_file);
-        }
-    }
-}
-
-pub fn searched_idf_include(b: *std.Build, lib: *std.Build.Step.Compile, idf_path: []const u8) !void {
-    const comp = b.pathJoin(&.{ idf_path, "components" });
-    var dir = try std.fs.cwd().openDir(comp, .{
-        .iterate = true,
-    });
-    defer dir.close();
-    var walker = try dir.walk(b.allocator);
-    defer walker.deinit();
-
-    while (try walker.next()) |entry| {
-        const ext = std.fs.path.extension(entry.basename);
-        const include_file = inline for (&.{".h"}) |e| {
-            if (std.mem.eql(u8, ext, e))
-                break true;
-        } else false;
-        if (include_file) {
-            const include_dir = b.pathJoin(&.{ comp, std.fs.path.dirname(b.dupe(entry.path)).? });
-            lib.addIncludePath(.{ .cwd_relative = include_dir });
-        }
-    }
 }
 
 pub fn idf_wrapped_modules(b: *std.Build) *std.Build.Module {
