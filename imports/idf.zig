4c41d4486cc868c2693e7f5f1e6d35f74f95cb59
Matheus C. França <matheus-catarino@hotmail.com>
2026-02-19 13:20:44 -0300

    Refactor import conditions for DSP and LED strip

--- imports/idf.zig
+++ imports/idf.zig
@@ -1,9 +1,9 @@
 pub const bl = @import("bootloader");
 pub const bt = @import("bluetooth");
 pub const crc = @import("crc");
-pub const dsp = switch (@as(bool, sys.HAS_ESP_DSP)) {
+pub const dsp = switch (sys.HAS_ESP_DSP != 0) {
     true => @import("dsp"),
-    else => @compileError("requires: idf.py add-component espressif/esp-dsp"),
+    false => @compileError("requires: idf.py add-component espressif/esp-dsp"),
 };
 pub const err = @import("error");
 pub const gpio = @import("gpio");
@@ -11,13 +11,14 @@ pub const heap = @import("heap");
 pub const http = @import("http");
 pub const i2c = @import("i2c");
 pub const i2s = @import("i2s");
-pub const led = switch (@as(bool, sys.HAS_ESP_LED_STRIP)) {
+pub const led = switch (sys.HAS_ESP_LED_STRIP != 0) {
     true => @import("led"),
-    else => @compileError("requires: idf.py add-component espressif/led-strip"),
+    false => @compileError("requires: idf.py add-component espressif/led_strip"),
 };
 pub const log = @import("log");
 pub const lwip = @import("lwip");
 pub const mqtt = @import("mqtt");
+pub const esp_now = @import("now");
 pub const phy = @import("phy");
 pub const pulse = @import("pulse");
 pub const esp_panic = @import("panic");
@@ -54,3 +55,35 @@ pub const currentTarget = blk: {
         @compileError("Unknown ESP32 device target: " ++ target_str);
     };
 };
+
+// Check all imports
+comptime {
+    _ = sys;
+    _ = bl;
+    _ = bt;
+    _ = crc;
+    _ = err;
+    _ = gpio;
+    _ = heap;
+    _ = http;
+    _ = i2c;
+    _ = i2s;
+    _ = log;
+    _ = lwip;
+    _ = mqtt;
+    _ = esp_now;
+    _ = phy;
+    _ = pulse;
+    _ = esp_panic;
+    _ = rtos;
+    _ = segger;
+    _ = spi;
+    _ = uart;
+    _ = ver;
+    _ = switch (currentTarget) {
+        .esp32h2, .esp32h21, .esp32h4, .esp32p4 => {},
+        else => wifi,
+    };
+    if (sys.HAS_ESP_DSP != 0) _ = dsp;
+    if (sys.HAS_LED_STRIP != 0) _ = led;
+}
